cad7e802e34dc24ae0eaedae8c9d162f281841c7
Yash Bakadiya <bakadiyayash@gmail.com>
2025-04-27 22:44:57 +0530

    Added Dark/light mode functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['selector', '[class="app-dark"]'],
   content: [
     './src/**/*.{html,ts,scss,css}',
     './index.html'
