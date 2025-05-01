6a48097eda0af3abd90ec2773848110388e130f9
Yash Bakadiya <bakadiyayash@gmail.com>
2025-05-01 17:04:17 +0530

    Restructured files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [{
   path: 'auth',
   loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
 },
 {
-  path: 'dashboard',
-  loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
-  canActivate: [authGuard]
+  path: 'pages',
+  loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
 },
-{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-{ path: '**', redirectTo: '/dashboard' }];
+{ path: '', redirectTo: '/pages', pathMatch: 'full' },
+{ path: '**', redirectTo: '/pages' }];
