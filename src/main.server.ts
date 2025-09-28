c3176ef3bc67323ac5e4244da1b3afcf2a165888
Yash Bakadiya <68679680+yashbakadiya@users.noreply.github.com>
2025-09-28 18:20:46 +0530

    Update main.server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,13 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideServerRendering } from '@angular/platform-server';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-export default function bootstrap() {
-  return bootstrapApplication(AppComponent, {
-    providers: [
-      provideServerRendering(),
-      ...config.providers,
-    ],
-  });
-}
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
+
+export default bootstrap;
